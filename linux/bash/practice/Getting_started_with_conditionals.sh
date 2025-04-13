e460020ca26dae57dc4e8f3cc5cafa8679416e50
Rajesh-Myada <myadarajesh2001@gmail.com>
2025-04-13 19:14:46 +0530

    created bash practice folder with some files and Prompt Engineering folder

---
+++ linux/bash/practice/Getting_started_with_conditionals.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+read ch#!/bin/bash
+
+read ch
+if [[ "$ch" == "Y" || "$ch" == "y" ]]
+then
+    echo "YES"
+else
+    echo "NO"
+fi
---
+++ linux/bash/practice/The_World_of_Numbers.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+read x
+read y
+echo $(expr $x + $y)
+echo $(expr $x - $y)
+echo $(expr $x \* $y)
+echo $(expr $x / $y)
